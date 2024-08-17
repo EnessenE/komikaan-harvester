873bf9316f3b556abae5c9c60d236b5723055a80
Enes <9263360+EnessenE@users.noreply.github.com>
2024-08-17 09:59:11 +0200

    0.5.1

---
+++ komikaan.Harvester/Migrations/20240817075447_AddShapeIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace komikaan.Harvester.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShapeIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "ix_shapes_id_data_origin_sequence",
+                table: "shapes",
+                columns: new[] { "id", "data_origin", "sequence" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ix_shapes_id_data_origin_sequence",
+                table: "shapes");
+        }
+    }
+}
