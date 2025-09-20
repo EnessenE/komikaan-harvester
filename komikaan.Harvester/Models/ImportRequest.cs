b6509092e22e08fc71c82dbb03096ad57d7a21ae
Enes <9263360+EnessenE@users.noreply.github.com>
2025-09-20 16:15:23 +0200

    2.1.3 - Support headers

--- komikaan.Harvester/Models/ImportRequest.cs
+++ komikaan.Harvester/Models/ImportRequest.cs
@@ -25,4 +25,6 @@ public class ImportRequest
 
     public DateTimeOffset ImportRequestedAt { get; set; }
     public TimeSpan DelayImportBy { get; set; } = TimeSpan.Zero;
+    public string? HeaderKey { get; set; }
+    public string? HeaderValue { get; set; }
 }
